f201f1182596c7d87863a52e3a23e80f1ad7eee6
Campbell McTernan <u7484835@anu.edu.au>
2024-04-21 03:32:58 +1000

    Added picture assets, finished preliminary classic map testing

--- riskai/testing.py
+++ riskai/testing.py
@@ -1,13 +1,24 @@
 from maps import *
 import networkx as nx
 import matplotlib.pyplot as plt
+import scipy as sp
 
 
 
+bonus_color_map = {
+        'NA': 'blue',
+        'SA': 'red',
+        'AF': 'purple',
+        'EU': 'green',
+        'AS': 'orange',
+        'AU': 'yellow'
+    }
 
 def drawTest():
     ax = plt.subplot(111)
-    nx.draw(Classic, with_labels=True, font_weight='bold')
+    node_colors = [bonus_color_map[Classic.nodes[node]['bonus']] for node in Classic.nodes()]
+    posConfig = nx.kamada_kawai_layout(Classic)
+    nx.draw(Classic, pos = posConfig, with_labels=True, font_weight='bold', node_color=node_colors)
     plt.savefig('ClassicGraphTest.png')
 
     
