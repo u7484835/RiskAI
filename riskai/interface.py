f2e32e7def1deb7ebd56e7fe1aacc51dcb12d51c
Campbell McTernan <u7484835@anu.edu.au>
2024-04-21 20:47:12 +1000

    finished initial interface and structures draft

--- riskai/interface.py
+++ riskai/interface.py
@@ -1,7 +1,7 @@
 # Will contain components for using click to transition between click input/output and variables to 
 # coded objects
 import click
-
+from structures import *
 
 
 
@@ -9,7 +9,7 @@ import click
 # First interface functions to be called, helps to initialise the game structures.
 
 
-def getMapInfo():
+def getMapInfo() -> MapType:
     """
     Asks the user what map the game is being played on.
 
@@ -18,16 +18,16 @@ def getMapInfo():
 
 
 
-def getBlizzardInfo():
+def getBlizzardInfo() -> bool:
     """
     Asks the user if the game has blizzards enabled.
     """
     pass
 
-
-def getFogInfo():
+# Likely won't implement
+def getCapitalsInfo() -> bool:
     """
-    Asks the user if the game has blizzards enabled.
+    Asks the user if the game has the capitals gamemode enabled.
     """
     pass
 
@@ -41,6 +41,7 @@ def getFogInfo():
 def getPlayersInfo():
     """
     Gets information about how many players there are, order of players, colours associated with players.
+    Should update main data structures. 
     """
     pass
 
@@ -48,18 +49,16 @@ def getPlayersInfo():
 def getTroopInfo():
     """
     Gets information the amount of troops on each territory, and the owner of each territory.
+    Should update main data structures.
     """
     pass
 
 
-# Gets information about how many players there are, order of players and which player owns what territories with what amount of troops. 
-# Updates main game variables to do this. Should provide checksum values (comparing troops inputted to troop tally provided by game)
-
-#Uses getPlayersInfo(), getTroopInfo()
 def getSetupInfo():
     """
     Gets information about how many players there are, order of players and which player owns what territories with what amount of troops. 
     Updates main game variables to do this. Should provide checksum values (comparing troops inputted to troop tally provided by game).
+    Should update main data structures.
     
     Requires:
         - `getPlayersInfo()`
@@ -70,14 +69,14 @@ def getSetupInfo():
 
 
 # !Note: The player parameter for all turninfo related functions may not be necesarry. It could be implicit. 
-def getDraftInfo():
+def getDraftInfo() -> Draft:
     """
     Gets which player drafted, how many troops they drafted, where they drafted, and whether they traded in cards.
     """
     pass
 
 
-def getAttackInfo():
+def getAttackInfo() -> Attack:
     """
     Gets where which player attacked, what territory was attacked, how many troopes were lost on each side, 
     how many troops were moved to the new terrtory.
@@ -85,18 +84,20 @@ def getAttackInfo():
     pass
 
 
-def getFortifyInfo():
+def getFortifyInfo() -> Fortify:
     """
     Gets which player fortified, where they fortified from, where they fortified to, and how many troops they fortified.
     """
     pass
 
 
-def getTurnInfo():
+def getTurn():
     """
     Gets info about the amount of troops a player drafted, where they played the draft,
     whether they traded in cards, what territories they attacked, whether they killed a player (needed or inferred?) 
     what territories they left troops in when attacking, and where they fortified to. 
+    
+    Should update internal data structures.
     
     
     Requires:
@@ -110,15 +111,22 @@ def getTurnInfo():
 
 # ---------------------------- Output - Game interface ----------------------------
 
-# Tells user where to draft troops, whether to trade cards, what cards to use.
-def instructDraft():
+
+def instructDraft(draft : Draft) :
+    """
+    Tells user where to draft troops, whether to trade cards, what cards to use.
+    """
     pass
 
-# Tells user which territories to attack with, where to attack, how many to attack with, and how many to move.
-def instructAttack():
+def instructAttack(attacks : Attack):
+    """
+    Tells user which territories to attack with, where to attack, how many to attack with, and how many to move.
+    """
     pass
 
-# Tells user whether to fortify, where fortify from, where to fortify to, and how many to fortify with.
-def instructFortify():
+def instructFortify(fortify : Fortify):
+    """
+    Tells user whether to fortify, where fortify from, where to fortify to, and how many to fortify with.
+    """
     pass
 
--- riskai/structures.py
+++ riskai/structures.py
@@ -1,20 +1,91 @@
+# Will contain datatypes and classes to be used in the AI section
+
 import networkx as nx
 from enum import Enum
+from typing import Optional, Tuple, TypeAlias
 
-class Card(Enum):
-    INFANTRY = 1
-    CAVALRY = 2
-    ARTILLERY = 3
-    WILD = 4
+class MapType(Enum):
+    """
+    Represents the different maps that can be played on.
+
+    Attributes:
+        CLASSIC: Original Risk map in physical board game.
+        ASIA: Called "Asia 1800's" in Risk: Global domination. 48 territories, features large indonesia bonus. 
+        US: Called "United States" in Risk: Global domination. 42 territories, features uniform blocky bonuses. 
+    """
+    CLASSIC = 1
+    ASIA = 2
+    US = 3
 
 
+class CardType(Enum):
+    """
+    Represents different card types gained after capturing at least 
+    one territory during a turn.
 
+    Attributes:
+        INFANTRY: Weakest card, 3 give 4 troops.
+        CAVALRY: Medium card, 3 give 6 troops.
+        ARTILLERY: Strong card, 3 give 8 troops.
+        WILD: Strongest card, can act as any of the above cards.
+    """
+    INFANTRY = 4
+    CAVALRY = 6
+    ARTILLERY = 8
+    WILD = 10
+
+class Card:
+    """
+    Represents a card that can be gained after capturing at least 
+    one territory during a turn.
+
+    Attributes:
+        type (CardType): The type of card eg Infantry
+        territory (int): The territory ID the card belongs to. 
+    """
+    def __init__(self, type : CardType, territory : int):
+        self.type = type
+        self.territory = territory
+
+
+Trade: TypeAlias = Optional[Tuple[CardType, CardType, CardType]]
+"""
+Represents whether a trade action is intending to be played. It will be 
+None if no trade is occuring, otherwise it will be a tuple of three cards 
+to be played in the order given. 
+"""
+
+
+# Should I keep as class or make type alias?
 class Draft:
-    def __init__(self, draft_id, draft_name, draft_type, draft_date, draft_status, draft_rounds, draft_teams):
-        self.draft_id = draft_id
-        self.draft_name = draft_name
-        self.draft_type = draft_type
-        self.draft_date = draft_date
-        self.draft_status = draft_status
-        self.draft_rounds = draft_rounds
-        self.draft_teams = draft_teams
+    """
+    Represents the draft phase of the player to be taken.
+
+    Attributes:
+        trade (Trade): The trade (if it will occur) and the associated cards.
+        drafts (list[(int, int)]): A list of locations formatted as (territory ID, amount of troops) to place troops.  
+
+    Methods:
+        __init__(name: str, age: int): Initializes a new instance of the Person class.
+        introduce(): Prints an introduction message for the person.
+    """
+    def __init__(self, trade: Trade, drafts : list[(int, int)]):
+        self.trade = trade
+        self.drafts = drafts
+
+
+Attack: TypeAlias = list[Tuple[int, int, int, int]]
+"""
+Represents what attacks user player should make. Each tuple in 
+list is one attack. Empty list means no attacks. ints are structured
+as (territoryID from, territoryID to, amount of troops to attack with, amount of troops to move).
+"""
+
+
+Fortify: TypeAlias = Optional[Tuple[int, int, int]]
+"""
+Represents what fortify user player should make. If None, no fortify.
+ints in tuple are structured as (territoryID from, territoryID to, amount of troops to move).
+"""
+
+
