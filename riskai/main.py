5b5de09d5735a753a0bf4df3674071cb4ffc31c3
Campbell McTernan <u7484835@anu.edu.au>
2024-04-23 16:00:40 +1000

    refactoring, saving "main" click implementation draft before removing.

--- riskai/main.py
+++
@@ -1,1 +0,0 @@
-print("Hello World")  #Do main things to run project
--- riskai/riskAi.py
+++ riskai/riskAi.py
@@ -3,57 +3,6 @@ import click
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @click.command(name="riskAI")
 @click.option("b", "--blizzards", type=bool, default=False, prompt= "Does the gamemode have blizards enabled?", help="The gamemode settings specifying blizzards.")
 @click.option("-f", "--fog", type=bool, default=False, prompt= "Does the gamemode have fog enabled?", help="The gamemode settings specifying fog.")
